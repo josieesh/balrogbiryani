f53e6a3e67c0ed205ab3aa4fb033b9f3562b7d21
josieesh <jeshkena@uwaterloo.ca>
2022-02-01 20:30:35 -0800

    hmm

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Word :numLetters="5"/>
+    <Word :word="word"/>
   </div>
 </template>
 
 <script>
 import Word from './components/Word.vue'
 
+//const Http = new XMLHttpRequest();
+const url='http://localhost:5000/randomword';
+
+const xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", url, false ); // false for synchronous request
+xmlHttp.send( null );
+var word = xmlHttp.responseText;
+console.log(word)
+
 export default {
   name: 'App',
   components: {
     Word
-  }
+  },
+  data() {
+   return {
+     word: word
+   }
+ }
 }
 </script>
 
--- webapp/src/components/Word.vue
+++ webapp/src/components/Word.vue
@@ -3,27 +3,28 @@
     <label for="name">{{ numLetters }} letters</label>
     <br>
     <input type="text" id="guess" name="guess" required
-       v-bind:minlength="numLetters" v-bind:maxlength="numLetters" size="14">
+        v-bind:minlength="numLetters" v-bind:maxlength="numLetters" size="14">
   </div>
 </template>
 
 <script>
-const Http = new XMLHttpRequest();
-const url='http://localhost:5000/randomword';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = function() {
-    if(this.readyState==4 && this.status==200){
-        console.log(Http.responseText)
-    }
-}
-
+// var numLetters = this.word.length()
+// document.getElementById("guess").innerHTML = "".concat(numLetters, " letters") 
+// document.getElementById("guess").setAttribute("minLength", numLetters);
+// document.getElementById("guess").setAttribute("maxLength", numLetters);
 export default {
   name: 'Word',
   props: {
-    numLetters: Number
-  }
+    word: String
+  },
+  data() {
+      return {
+        numLetters: 0
+      }
+  },
+  created() {
+           this.numLetters = this.word.length;
+        }
 }
 </script>
 
