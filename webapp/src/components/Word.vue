0a2cfaf8df6dd2c7d2f6ebac98287dc80ec54aa4
josieesh <jeshkena@uwaterloo.ca>
2022-02-06 21:30:17 -0800

    checking and color change

--- webapp/src/components/Word.vue
+++ webapp/src/components/Word.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="word">
-    <label for="name">{{ numLetters }} letters</label>
-    <br>
-    <input type="text" id="guess" name="guess" required
-        v-bind:minlength="numLetters" v-bind:maxlength="numLetters" size="14">
+    <form @submit.prevent="submit" id="form">
+        <label for="name">{{ numLetters }} letters</label>
+        <br>
+        <input type="text" id="guess" v-model="guess" required
+            v-bind:minlength="numLetters" v-bind:maxlength="numLetters" size="14"
+             v-on:input="clear">
+    </form>
   </div>
 </template>
 
@@ -17,21 +20,51 @@ export default {
   props: {
     word: String
   },
+  methods : {
+    submit : function(){
+        if (this.guess == this.word) {
+            console.log("correct!");
+            document.getElementById("guess").className = "correct";
+        } else {
+            document.getElementById("guess").className = "wrong";
+        }
+    },
+    clear: function() {
+        document.getElementById("guess").className = "";
+    }
+  },
   data() {
       return {
-        numLetters: 0
+        numLetters: 0,
+        guess: ""
       }
   },
   created() {
-           this.numLetters = this.word.length;
+      this.numLetters = this.word.length;
+  },
+  afterMount(){
+    const guess = document.getElementById("guess");
+    guess.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("form").submit();
         }
+    });
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+input.wrong, textarea { 
+    background: red; 
+}
+input.correct, textarea { 
+    background: cyan;
+}
 input {
 text-align: center;
+background: white;
 }
 h3 {
   margin: 40px 0 0;
